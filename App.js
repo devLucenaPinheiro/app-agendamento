535a3802c80f8e6fa414320882e4f5dcaa7d43b0
Vinicius Lucena <131610721+devLucenaPinheiro@users.noreply.github.com>
2024-11-12 20:04:21 -0300

    ajuste no login
    agora o usuário pode ver apenas os seus agendamentos.

--- App.js
+++ App.js
@@ -1,83 +1,175 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { View, Text, TextInput, Alert, StyleSheet, TouchableOpacity, ScrollView} from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
 import { Calendar } from 'react-native-calendars'
 import { Picker } from '@react-native-picker/picker'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { registerUser, loginUser } from './database'
+import { Checkbox } from 'react-native-paper'
 
 const Tab = createBottomTabNavigator()
 
 const availableTimes = [
-  '12:00', '12:15', '12:30', '12:45', '13:00', '13:15', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45',
-  '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:15', '17:30', '17:45',
-  '18:00', '18:15', '18:30', '18:45', '19:00', '19:15', '19:30', '19:45', '20:00'
+  '12:00',
+  '12:15',
+  '12:30',
+  '12:45',
+  '13:00',
+  '13:15',
+  '13:30',
+  '13:45',
+  '14:00',
+  '14:15',
+  '14:30',
+  '14:45',
+  '15:00',
+  '15:15',
+  '15:30',
+  '15:45',
+  '16:00',
+  '16:15',
+  '16:30',
+  '16:45',
+  '17:00',
+  '17:15',
+  '17:30',
+  '17:45',
+  '18:00',
+  '18:15',
+  '18:30',
+  '18:45',
+  '19:00',
+  '19:15',
+  '19:30',
+  '19:45',
+  '20:00',
 ]
 
 const services = ['Corte de cabelo', 'Luzes no cabelo', 'Sobrancelha simples']
 
-function CalendarScreen({ events, selectedDate, setSelectedDate, newEvent, setNewEvent, addEvent, removeEvent }) {
+function CalendarScreen({
+  events,
+  selectedDate,
+  setSelectedDate,
+  addEvent,
+  removeEvent,
+}) {
   const [selectedTime, setSelectedTime] = useState('')
-  const [selectedService, setSelectedService] = useState('')
+  const [selectedServices, setSelectedServices] = useState([])
 
   const handleDatePress = (day) => {
     setSelectedDate(day.dateString)
   }
 
- const handleAddEvent = () => {
-  if (selectedTime && selectedService) {
-    addEvent(selectedDate, `${selectedService} às ${selectedTime}`)
-    setSelectedTime('')
-    setSelectedService('')
-  } else {
-    Alert.alert('Erro', 'Selecione um horário e um serviço.')
+  const handleAddEvent = () => {
+    if (selectedTime && selectedServices.length > 0) {
+      const eventDetails =
+        selectedServices.length === 1
+          ? `${selectedServices[0]} às ${selectedTime}`
+          : `${selectedServices.slice(0, -1).join(', ')} e ${
+              selectedServices[selectedServices.length - 1]
+            } às ${selectedTime}`
+
+      addEvent(selectedDate, eventDetails)
+      setSelectedTime('')
+      setSelectedServices([])
+    } else {
+      Alert.alert('Erro', 'Selecione um horário e pelo menos um serviço.')
+    }
   }
-}
+
+  const toggleServiceSelection = (service) => {
+    setSelectedServices((prev) =>
+      prev.includes(service)
+        ? prev.filter((s) => s !== service)
+        : [...prev, service]
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <Calendar onDayPress={handleDatePress} theme={calendarTheme} />
+      <Calendar
+  onDayPress={handleDatePress}
+  theme={{
+    backgroundColor: '#ffffff',
+    calendarBackground: '#ffffff',
+    textSectionTitleColor: '#b6c1cd',
+    selectedDayBackgroundColor: '#00adf5',
+    selectedDayTextColor: '#ffffff',
+    todayTextColor: '#00adf5',
+    dayTextColor: '#2d4150',
+    textDisabledColor: '#d9e1e8',
+    dotColor: '#00adf5',
+    selectedDotColor: '#ffffff',
+    arrowColor: 'orange',
+    monthTextColor: 'blue',
+    indicatorColor: 'blue',
+    textDayFontFamily: 'monospace',
+    textMonthFontFamily: 'monospace',
+    textDayHeaderFontFamily: 'monospace',
+    textDayFontWeight: '300',
+    textMonthFontWeight: 'bold',
+    textDayHeaderFontWeight: '300',
+    textDayFontSize: 16,
+    textMonthFontSize: 16,
+    textDayHeaderFontSize: 16
+  }}
+/>
       {selectedDate ? (
         <View style={styles.eventContainer}>
           <Text style={styles.eventDate}>Eventos em {selectedDate}:</Text>
           {events[selectedDate]?.map((event, index) => (
             <View key={index} style={styles.eventItem}>
               <Text style={styles.eventText}>• {event}</Text>
-              <TouchableOpacity onPress={() => removeEvent(index)} style={styles.removeButton}>
+              <TouchableOpacity
+                onPress={() => removeEvent(selectedDate, index)}
+                style={styles.removeButton}>
                 <Text style={styles.removeButtonText}>Desmarcar</Text>
               </TouchableOpacity>
             </View>
           ))}
+
           <Text style={styles.label}>Selecione o horário:</Text>
           <Picker
             selectedValue={selectedTime}
             onValueChange={(itemValue) => setSelectedTime(itemValue)}
-            style={styles.picker}
-          >
+            style={styles.picker}>
             <Picker.Item label="Escolha o horário" value="" />
             {availableTimes.map((time) => (
               <Picker.Item key={time} label={time} value={time} />
             ))}
           </Picker>
-          <Text style={styles.label}>Selecione o serviço:</Text>
-          <Picker
-            selectedValue={selectedService}
-            onValueChange={(itemValue) => setSelectedService(itemValue)}
-            style={styles.picker}
-          >
-            <Picker.Item label="Escolha o serviço" value="" />
-            {services.map((service) => (
-              <Picker.Item key={service} label={service} value={service} />
-            ))}
-          </Picker>
+
+          <Text style={styles.label}>Selecione o(s) serviço(s):</Text>
+          {services.map((service) => (
+            <View key={service} style={styles.checkboxContainer}>
+              <Checkbox
+                status={
+                  selectedServices.includes(service) ? 'checked' : 'unchecked'
+                }
+                onPress={() => toggleServiceSelection(service)}
+              />
+              <Text style={styles.checkboxLabel}>{service}</Text>
+            </View>
+          ))}
 
           <TouchableOpacity style={styles.button} onPress={handleAddEvent}>
             <Text style={styles.buttonText}>Salvar Evento</Text>
           </TouchableOpacity>
         </View>
       ) : (
-        <Text style={styles.infoText}>Selecione uma data para agendar um evento.</Text>
+        <Text style={styles.infoText}>
+          Selecione uma data para agendar um evento.
+        </Text>
       )}
     </View>
   )
@@ -93,7 +185,9 @@ function ScheduledEventsScreen({ events, handleLogout }) {
             <View key={date} style={styles.eventContainer}>
               <Text style={styles.eventDate}>Eventos em {date}:</Text>
               {eventList.map((event, index) => (
-                <Text key={index} style={styles.eventText}>• {event}</Text>
+                <Text key={index} style={styles.eventText}>
+                  • {event}
+                </Text>
               ))}
             </View>
           ))
@@ -108,7 +202,6 @@ function ScheduledEventsScreen({ events, handleLogout }) {
   )
 }
 
-
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   const [isRegistering, setIsRegistering] = useState(false)
@@ -117,6 +210,19 @@ export default function App() {
   const [password, setPassword] = useState('')
   const [selectedDate, setSelectedDate] = useState('')
   const [events, setEvents] = useState({})
+
+  useEffect(() => {
+    const loadEvents = async () => {
+      const storedEvents = await AsyncStorage.getItem(username)
+      if (storedEvents) {
+        setEvents(JSON.parse(storedEvents))
+      }
+    }
+
+    if (isAuthenticated) {
+      loadEvents()
+    }
+  }, [isAuthenticated, username])
 
   const handleLogin = async () => {
     const success = await loginUser(username, password)
@@ -142,23 +248,30 @@ export default function App() {
     }
   }
 
-   const addEvent = (date, event) => {
-    setEvents({
+  const addEvent = async (date, event) => {
+    const updatedEvents = {
       ...events,
       [date]: [...(events[date] || []), event],
-    })
+    }
+    setEvents(updatedEvents)
+    await AsyncStorage.setItem(username, JSON.stringify(updatedEvents))
   }
 
-  const removeEvent = (index) => {
-    const updatedEvents = events[selectedDate].filter((_, i) => i !== index)
-    setEvents({
-      ...events,
-      [selectedDate]: updatedEvents,
-    })
+  const removeEvent = async (date, index) => {
+    const updatedEvents = { ...events }
+    updatedEvents[date].splice(index, 1)
+    if (updatedEvents[date].length === 0) {
+      delete updatedEvents[date]
+    }
+    setEvents(updatedEvents)
+    await AsyncStorage.setItem(username, JSON.stringify(updatedEvents))
   }
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
     setIsAuthenticated(false)
+    setUsername('')
+    setPassword('')
+    setEvents({})
   }
 
   if (!isAuthenticated) {
@@ -168,29 +281,37 @@ export default function App() {
         {isRegistering && (
           <TextInput
             placeholder="Nome"
+            style={styles.input}
             value={name}
             onChangeText={setName}
-            style={styles.input}
           />
         )}
         <TextInput
-          placeholder="Nome de Usuário"
+          placeholder="Usuário"
+          style={styles.input}
           value={username}
           onChangeText={setUsername}
-          style={styles.input}
         />
         <TextInput
           placeholder="Senha"
+          style={styles.input}
           value={password}
           onChangeText={setPassword}
           secureTextEntry
-          style={styles.input}
         />
-        <TouchableOpacity style={styles.button} onPress={isRegistering ? handleRegister : handleLogin}>
-          <Text style={styles.buttonText}>{isRegistering ? 'Cadastrar' : 'Entrar'}</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={isRegistering ? handleRegister : handleLogin}>
+          <Text style={styles.buttonText}>
+            {isRegistering ? 'Cadastrar' : 'Entrar'}
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => setIsRegistering(!isRegistering)}>
-          <Text style={styles.linkText}>{isRegistering ? 'Já tem uma conta? Login' : 'Não tem uma conta? Cadastre-se'}</Text>
+          <Text style={styles.switchText}>
+            {isRegistering
+              ? 'Já tem uma conta? Entrar'
+              : 'Ainda não tem uma conta? Cadastrar'}
+          </Text>
         </TouchableOpacity>
       </View>
     )
@@ -199,32 +320,30 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Calendário">
-  {() => (
-    <CalendarScreen
-      events={events}
-      selectedDate={selectedDate}
-      setSelectedDate={setSelectedDate}
-
-      addEvent={addEvent}
-      removeEvent={removeEvent}
-      handleLogout={handleLogout}
-    />
-  )}
-</Tab.Screen>
-
-        <Tab.Screen name="Horários Agendados">
-          {() => <ScheduledEventsScreen events={events} handleLogout={handleLogout} />}
-        </Tab.Screen>
+        <Tab.Screen
+          name="Agenda"
+          children={() => (
+            <CalendarScreen
+              events={events}
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+              addEvent={addEvent}
+              removeEvent={removeEvent}
+            />
+          )}
+        />
+        <Tab.Screen
+          name="Meus Agendamentos"
+          children={() => (
+            <ScheduledEventsScreen
+              events={events}
+              handleLogout={handleLogout}
+            />
+          )}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
-}
-
-const calendarTheme = {
-  selectedDayBackgroundColor: '#6b52ae',
-  todayTextColor: '#6b52ae',
-  arrowColor: '#6b52ae',
 }
 
 const styles = StyleSheet.create({
@@ -255,20 +374,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     alignItems: 'center',
   },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  linkText: {
-    color: '#6b52ae',
-    textAlign: 'center',
-    marginTop: 10,
-    textDecorationLine: 'underline',
-  },
-  eventContainer: {
-    marginTop: 20,
-  },
+  buttonText: { color: '#fff', fontSize: 16, fontWeight: 'bold' },
+  eventContainer: { marginTop: 20 },
   eventDate: {
     fontSize: 18,
     fontWeight: 'bold',
@@ -281,24 +388,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginVertical: 5,
   },
-  eventText: {
+  eventText: { fontSize: 16, color: '#333' },
+  removeButton: { paddingHorizontal: 10 },
+  removeButtonText: { color: 'red', fontSize: 14 },
+  infoText: { fontSize: 16, color: '#666', textAlign: 'center', marginTop: 20 },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    flexWrap: 'wrap',
+  },
+  checkboxLabel: {
     fontSize: 16,
-    color: '#333',
+    marginLeft: 10,
+    flexShrink: 1,
+    maxWidth: '80%',
   },
-  removeButton: {
-    paddingHorizontal: 10,
-  },
-  removeButtonText: {
-    color: 'red',
-    fontSize: 14,
-  },
-  infoText: {
-    fontSize: 16,
-    color: '#666',
-    textAlign: 'center',
-    marginTop: 20,
-  },
-logoutButton: {
+  label: { fontSize: 16, marginBottom: 10, color: '#333' },
+  logoutButton: {
     alignSelf: 'center',
     width: '90%',
     padding: 15,
